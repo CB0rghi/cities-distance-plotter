f7244c68ffb63e7b99069be2f683003ff5cc65ae
Caio Borghi <cborghi@tmrw.org>
2022-02-27 18:01:52 -0300

    Add visualization of graph

--- client/src/components/NearestRoutePlotter/NearestRoutePlotter.jsx
+++ client/src/components/NearestRoutePlotter/NearestRoutePlotter.jsx
@@ -7,35 +7,62 @@ export const background = '#272b4d';
 const ShortestRoutePlotter = () => {
   const { loading, sourceCity, shortestRoute} = useCityStore(state => ({ sourceCity: state.sourceCity, loading: state.loading, shortestRoute: state.shortestRoute}))
   const [elements, setElements] = useState([])
-  console.log('Plotter')
 
   useEffect(() => {
     if(!shortestRoute) return
-    const baseX = 450 
-    const dislocationY = 60
+    const baseX = 50 
+    const baseY = 10
+    const dislocationX = 180
+    const dislocationY = 80
     const cities = shortestRoute.split('=>').map(city => city.trim())
-    const mountElements = () => {
-      const elements = []
-      const firstItem = cities[0]
-      elements.push({
+
+    const firstNode = cities[0]
+    const firstVertice = {
         id: '0',
         type: 'input',
-        data: { label: firstItem },
-        position: { x: baseX, y: 0 }
-      })
+        data: { label: firstNode },
+        position: {
+          x: baseX, y: baseY
+        }
+    }
+    
+    const mountElements = () => {
+      const elements = [firstVertice]
+      const addNode = (city, index, x, y) => {
+        const node = {
+          id: index.toString(),
+          type: index === cities.length - 1 ? 'output' : 'default',
+          data: { label: city },
+          position: { x, y }
+        }
+        elements.push(node)
+      }
+      let currentX = baseX
+      let currentY = baseY
+      let positive = true
 
       for(let i = 1; i < cities.length; i++) {
         const current = cities[i]
-        elements.push({ 
-          id: i.toString(),
-          type: i === cities.length - 1 ? 'output' : 'default',
-          data: { label: current },
-          position: { x: baseX, y: dislocationY * i }
-        })        
+        if(i % 5 !== 0) {
+          if(positive) {
+            currentX += dislocationX
+          } else {
+            currentX -= dislocationX
+          }
+        }
+        else {
+          positive = !positive
+          currentY += dislocationY
+        }
+        addNode(current, i, currentX, currentY)
+
         const edge = {
           id: `e-${i-1}${i}`,
-          source: i - 1,
-          target: i
+          source: (i - 1).toString(),
+          target: i.toString(),
+          style: { stroke: '#000' },
+          type: 'smoothstep',
+          animated: true
         }
         elements.push(edge)
       }      
